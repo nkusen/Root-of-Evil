2d54f9368888737f94227c2d0e0b225f9945f901
Antonio <sambolek.antonio@gmail.com>
2025-01-28 19:07:57 +0100

    FPS counter

---
+++ Assets/Scripts/FPSDisplay.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class FPSDisplay : MonoBehaviour
+{
+    private float deltaTime = 0.0f;
+
+    void Update()
+    {
+        // Calculate the time between frames
+        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+    }
+
+    void OnGUI()
+    {
+        // Calculate FPS
+        float fps = 1.0f / deltaTime;
+
+        // Display FPS on the screen
+        int width = Screen.width, height = Screen.height;
+        GUIStyle style = new GUIStyle();
+
+        Rect rect = new Rect(0, 0, width, height * 2 / 100);
+        style.alignment = TextAnchor.UpperLeft;
+        style.fontSize = height * 2 / 50;
+        style.normal.textColor = Color.white;
+
+        string text = $"FPS: {fps:0.}";
+        GUI.Label(rect, text, style);
+    }
+}
